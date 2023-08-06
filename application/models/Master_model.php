becd4f126c00f6e5cf21b6061d73434fad32ab0c
tawhid12 <tawhid99@gmail.com>
2023-08-06 19:49:13 +0600

    correction

--- application/models/Master_model.php
+++ application/models/Master_model.php
@@ -33,6 +33,8 @@ class Master_model extends CI_Model {
         $this->db->order_by('news_id', 'DESC');
         $query_result = $this->db->get();
         $res = $query_result->row();
+        echo '<pre>';
+        print_r($res);die;
         return $res;
     }
 	
