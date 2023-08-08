d199c8e51cee5f1614c87754e066c3da0925f33a
tawhid12 <tawhid99@gmail.com>
2023-08-08 23:28:20 +0600

    correction

--- application/views/frontend/pages/category_view.php
+++ application/views/frontend/pages/category_view.php
@@ -40,7 +40,7 @@
       //} ?>
 
       <?php foreach ($all_blog_data as $abd) { ?>
-        <div class="col-sm-4 col-md-3 mb-3">
+        <!-- <div class="col-sm-4 col-md-3 mb-3">
           <a class="text-decoration-none" href="<?= base_url() . "product-view/" . $abd->category_name . "/" . $abd->news_name . "/" . $abd->news_id . "/" . $abd->fk_news_id; ?>">
             <div class="card shadow">
               <img src="<?= base_url() . $abd->news_image; ?>" class="card-img-top" alt="..." width="100%" style="height:203px !important" />
@@ -49,7 +49,7 @@
               </div>
             </div>
           </a>
-        </div>
+        </div> -->
       <?php } ?>
     </div>
   </div>
