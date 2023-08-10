224fb0df20b548f959edd0e93ef81bf2dc6b1b98
tawhid12 <tawhid99@gmail.com>
2023-08-10 16:12:57 +0600

    correction

--- application/views/frontend/pages/contact_us.php
+++ application/views/frontend/pages/contact_us.php
@@ -74,7 +74,7 @@
               </div>
               <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Your Company Name</label>
-                <input type="email" class="form-control" name="contact_subject" id="exampleFormControlInput1" placeholder="ABC Inc." />
+                <input type="text" class="form-control" name="contact_subject" id="exampleFormControlInput1" placeholder="ABC Inc." />
               </div>
               <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Massege</label>
